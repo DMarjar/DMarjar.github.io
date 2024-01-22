be275bce2584a7e46cc7b3b6d75665459fa9475d
DMarjar <20213tn119@utez.edu.mx>
2024-01-22 13:06:20 -0600

    final

--- coordinates.js
+++ coordinates.js
@@ -1,89 +1,142 @@
-const xCoord = document.getElementById("xCoord");
-const yCoord = document.getElementById("yCoord");
+const MaxXCoord = document.getElementById("xCoord");
+const MaxYCoord = document.getElementById("yCoord");
 const points = document.getElementById("points");
 const time = document.getElementById("time");
-const startButton = document.getElementById("startButton");
-const stopButton = document.getElementById("stopButton");
 
-function createArray() {
-    const array = [xCoord.value][yCoord.value];
-    const coord = coordinatesGenerator(points.value);
+const table = document.getElementById("table");
+let markedPoints = [];
 
-    let canPaint = true;
-    for (let i = 0; i < coord.length; i++) {
-        if (array[coord[i].x][coord[i].y] === 'X') {
-            canPaint = false;
-            break;
-        } 
-    }
+let intervalId;
 
-    if (canPaint) {
-        for (let i = 0; i < coord.length; i++) {
-            array[coord[i].x][coord[i].y] = 'X';
-        }
-    }
+// Validates the input values
+function validate() {
+  if (
+    MaxXCoord.value == "" ||
+    MaxYCoord.value == "" ||
+    points.value == "" ||
+    time.value == ""
+  ) {
+    alert("Please, insert valid values!");
+    return false;
+  }
+
+  if (
+    MaxXCoord.value == NaN ||
+    MaxYCoord.value == NaN ||
+    points.value == NaN ||
+    time.value == NaN
+  ) {
+    alert("Please, insert valid values!");
+    return false;
+  }
+
+  if (
+    MaxXCoord.value <= 0 ||
+    MaxYCoord.value <= 0 ||
+    points.value <= 0 ||
+    time.value <= 0
+  ) {
+    alert("Please, insert valid values!");
+    return false;
+  }
+
+  if (points.value > MaxXCoord.value * MaxYCoord.value) {
+    alert("The number of points can't be greater than the number of cells!");
+    return false;
+  }
+
+  return true;
 }
 
+// Function to start the coordinate marking
+function start() {
+  if (!validate()) {
+    return;
+  }
+  // Generates the table with the given coordinates
+  generateTable(MaxXCoord.value, MaxYCoord.value);
+  // Clears the marked points array
+  markedPoints = [];
+  // Begins the interval to mark the coordinates
+  intervalId = setInterval(() => {
+    markCoordinates(MaxYCoord.value, MaxYCoord.value, points.value);
+  }, time.value * 1000);
+}
 
-function coordinatesGenerator(numberOfPoints) {
-  const generatedCoordinates = [numberOfPoints];
+// Function to stop the coordinate marking
+function stop() {
+  clearInterval(intervalId);
+}
+
+// Function to generate the table with the given coordinates
+function generateTable(xCoord, yCoord) {
+  // Clears the table
+  table.innerHTML = "";
+  // Generates the table
+  for (let i = 0; i < yCoord; i++) {
+    // Creates a row in the table
+    const row = table.insertRow();
+    for (let j = 0; j < xCoord; j++) {
+      // Creates a cell in the row
+      const cell = row.insertCell();
+      // Generates an unique id for the cell using the coordinates
+      cell.id = "td" + (i + 1) + (j + 1);
+    }
+  }
+}
+
+// Function to mark the coordinates
+function markCoordinates(xCoord, yCoord, pointValue) {
+  // Generates the coordinates to be possibly marked
+  const generatedCoordinates = generateRandomCoordinates(
+    xCoord,
+    yCoord,
+    pointValue
+  );
+  // Checks if the coordinates can be marked
+  let canPaint = true;
+  // Array to store the possible marks
+  let possibleMarks = [];
+
+  // Checks if the coordinates can be marked
   for (let i = 0; i < generatedCoordinates.length; i++) {
-    let x = Math.floor(Math.random() * xCoord.value + 1);
-    let y = Math.floor(Math.random() * yCoord.value + 1);
+    let x = generatedCoordinates[i].x;
+    let y = generatedCoordinates[i].y;
+    let cell = document.getElementById("td" + y + x);
+
+    // Checks if the cell is already marked
+    if (!markedPoints.includes(cell)) {
+      // If not, adds the cell to the possible marks array
+      possibleMarks.push(cell);
+    } else {
+      // If yes, sets the canPaint variable to false and breaks the loop
+      canPaint = false;
+      possibleMarks = [];
+      break;
+    }
+  }
+
+  // If the array of possible marks is not empty and the coordinates can be marked, marks the coordinates
+  if (possibleMarks.length > 0 && canPaint) {
+    for (let i = 0; i < possibleMarks.length; i++) {
+      // Marks the cell
+      possibleMarks[i].className = "marked";
+      // Adds the cell to the marked points array
+      markedPoints.push(possibleMarks[i]);
+    }
+  }
+}
+
+// Function to generate random coordinates
+function generateRandomCoordinates(xCoord, yCoord, pointValue) {
+  // Array to store the generated coordinates
+  const generatedCoordinates = [pointValue];
+  // Generates the coordinates
+  for (let i = 0; i < pointValue; i++) {
+    let x = Math.floor(Math.random() * xCoord) + 1;
+    let y = Math.floor(Math.random() * yCoord) + 1;
+    // Adds the coordinates to the array in an object
     generatedCoordinates[i] = { x, y };
   }
   return generatedCoordinates;
-}
-
-// AQUI TERMINA EL CODIGO QUE HIZO NARVIK //
-
-// AQUI EMPIEZA EL CODIGO QUE HICE YO //
-
-function createTable() {
-  // If the table already exists dont create it again
-  if (document.getElementById("table")) {
-    return;
-  }
-  // Create the table
-  const table = document.createElement("table");
-  table.id = "table";
-
-  for (let i = 0; i < xCoord.value; i++) {
-    let tr = document.createElement("tr");
-    for (let j = 0; j < yCoord.value; j++) {
-      let td = document.createElement("td");
-      td.id = "td" + (i + 1) + (j + 1);
-      tr.appendChild(td);
-    }
-    table.appendChild(tr);
-  }
-  const tableContainer = document.getElementById("table-container");
-  table.className = "table-primary";
-  tableContainer.appendChild(table);
-
-  runProgram();
-}
-
-function runProgram() {
-  const time = parseInt(document.getElementById("time").value);
-  const points = parseInt(document.getElementById("points").value);
-  const MaxXCoord = parseInt(document.getElementById("xCoord").value);
-  const MaxYCoord = parseInt(document.getElementById("yCoord").value);
-
-  console.log("Running program");
-  // For each point, generate a random coordinate
-  for (let i = 0; i < points; i++) {
-    // Generate a random number for X
-    let xCoord = Math.floor(Math.random() * MaxXCoord + 1);
-    // Generate a random number for Y
-    let yCoord = Math.floor(Math.random() * MaxYCoord + 1);
-    // Set the random coordinates to the table
-    let td = document.getElementById("td" + xCoord + yCoord);
-    if (td.className === "marked") {
-      console.log("Already marked");
-      continue;
-    } else {
-      td.className = "marked";
-    }
-  }
 }
